132327809e2d3579a9f8cab37391c315b8a3bec9
racko92 <vladimir.rackovic.92@gmail.com>
2017-09-06 18:33:26 +0200

    Publish konfiguracije

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,7 @@
 
 use Illuminate\Http\Request;
 
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
